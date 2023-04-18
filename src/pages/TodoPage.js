cdb7a3bc4611b1bcc18c715f8314ffeca1abaeaf
Jungkyu Yang <didwjd8590412@gmail.com>
2023-04-18 20:08:59 +0900

    feat(assignment6): todo 추가 기능 구현
    새로운 toodo가 서버에 등록 되도록 구현

--- src/pages/TodoPage.js
+++ src/pages/TodoPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import TodoList from "../components/TodoList";
@@ -6,17 +6,65 @@ import TodoList from "../components/TodoList";
 function TodoPage() {
   const navigate = useNavigate();
 
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
   useEffect(() => {
     if (!localStorage.getItem("access_token")) {
       return navigate("/signin");
     }
   }, [navigate]);
 
+  const createTodo = async () => {
+    const url = "https://www.pre-onboarding-selection-task.shop/todos";
+    const token = localStorage.getItem("access_token");
+    const requetOptions = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ todo: newTodo }),
+    };
+    try {
+      const response = await fetch(url, requetOptions);
+
+      if (!response.ok) {
+        throw new Error("Todo 생성 요청을 보내는데 실패했습니다.");
+      }
+      const data = await response.json();
+      setTodoList([...todoList, data]);
+      setNewTodo("");
+    } catch (error) {
+      console.error("Todo Error:", error);
+    }
+  };
+
   return (
-    <ul>
-      <TodoList todo="TODO 1" isCompleted={false} />
-      <TodoList todo="TODO 2" isCompleted={true} />
-    </ul>
+    <>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => {
+            setNewTodo(e.target.value);
+          }}
+          placeholder="Todo를 입력해주세요."
+          data-testid="new-todo-input"
+        ></input>
+        <button
+          type="button"
+          data-testid="new-todo-add-button"
+          onClick={createTodo}
+        >
+          추가
+        </button>
+      </div>
+      <ul>
+        <TodoList todo="TODO 1" isCompleted={false} />
+        <TodoList todo="TODO 2" isCompleted={true} />
+      </ul>
+    </>
   );
 }
 
