d4fdd874b763dc490437bc89e12689f8c4ceb9aa
Jungkyu Yang <didwjd8590412@gmail.com>
2023-04-18 17:39:42 +0900

    feat: 라우팅 및 NotFoundPage 컴포넌트 추가
    main, signup, signin, todo 페이지를 위한 라우팅 추가
    존재하지 않는 경로를 처리하기 위해 NotFound 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import MainPage from "./pages/MainPage";
+import SignUpPage from "./pages/SignUpPage";
+import SignInPage from "./pages/SignInPage";
+import TodoPage from "./pages/TodoPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const ROUTES = {
+  MAIN: "/",
+  SIGNUP: "/signup",
+  SIGNIN: "/signin",
+  TODO: "/todo",
+};
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.MAIN} element={<MainPage />} />
+        <Route path={ROUTES.SIGNUP} element={<SignUpPage />} />
+        <Route path={ROUTES.SIGNIN} element={<SignInPage />} />
+        <Route path={ROUTES.TODO} element={<TodoPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
